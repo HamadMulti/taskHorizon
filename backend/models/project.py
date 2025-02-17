eb584691c4c4f774caff59e12a31542427f15f7a
HamadMulti <informhammadahmad@gmail.com>
2025-02-17 13:52:11 +0300

    project schema model

---
+++ backend/models/project.py
@@ -0,0 +1,7 @@
+from . import db
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey("user.id"))
