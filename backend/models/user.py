6b456ddcd0e68261b0a2789d979fa90d75e63544
HamadMulti <informhammadahmad@gmail.com>
2025-02-17 13:52:49 +0300

    user schema model

---
+++ backend/models/user.py
@@ -0,0 +1,14 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    otp = db.Column(db.String(6), nullable=True)
+    role = db.Column(db.String(10), default="user")
+    phone = db.Column(db.String(20), nullable=True)
+    location = db.Column(db.String(100), nullable=True)
+    gender = db.Column(db.String(10), nullable=True)
+    primary_email = db.Column(db.String(120), nullable=True)
+
