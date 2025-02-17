38927352efe2f1d71c194850904c6f0fedf62558
HamadMulti <informhammadahmad@gmail.com>
2025-02-18 01:14:42 +0300

    added more mailing options

--- backend/utils/mailer.py
+++ backend/utils/mailer.py
@@ -1,12 +1,21 @@
+import os
 from flask_mail import Message
 from app import mail
 
+my_email = os.getenv("MAIL_USERNAME")
+
 def send_otp_email(email, otp):
-    msg = Message("Your OTP Code", sender="your-email@gmail.com", recipients=[email])
+    msg = Message("Your OTP Code", sender=my_email, recipients=[email])
     msg.body = f"Your OTP Code is {otp}"
     mail.send(msg)
 
 def send_reset_email(email, token):
-    msg = Message("Password Reset Link", sender="your-email@gmail.com", recipients=[email])
+    msg = Message("Password Reset Link", sender=my_email, recipients=[email])
     msg.body = f"Click the link to reset your password: http://localhost:3000/reset-password/{token}"
+    mail.send(msg)
+
+
+def new_registration_email(email):
+    msg = Message("Welcome to our platform", sender=my_email, recipients=[email])
+    msg.body = "Welcome to our platform. We are glad to have you here."
     mail.send(msg)
