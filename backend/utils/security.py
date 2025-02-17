ad6e9126025b81eb99724b42eeb9db9baadc7615
HamadMulti <informhammadahmad@gmail.com>
2025-02-18 01:15:08 +0300

    added decoded message

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,13 +1,25 @@
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, decode_token
 
 bcrypt = Bcrypt()
+
 
 def hash_password(password):
     return bcrypt.generate_password_hash(password).decode("utf-8")
 
+
 def verify_password(hashed_password, password):
     return bcrypt.check_password_hash(hashed_password, password)
 
+
 def generate_token(user_id):
     return create_access_token(identity=user_id)
+
+
+def decoded_token(token):
+    try:
+        decoded = decode_token(token)
+        return decoded['identity']
+    except Exception as e:
+# sourcery skip: raise-specific-error
+        raise Exception(f"Invalid token: {str(e)}") from e
