109cff6631fa1bb44ff933d43e068b3b5a2cdc84
HamadMulti <informhammadahmad@gmail.com>
2025-02-20 06:46:25 +0300

    added user route

--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
-from controllers.user_controller import update_profile
+from controllers.user_controller import get_profile, update_profile
 
 user_bp = Blueprint("user", __name__)
 
 user_bp.route("/update-profile", methods=["PUT"])(update_profile)
+user_bp.route("/profile", methods=["GET"])(get_profile)
+
