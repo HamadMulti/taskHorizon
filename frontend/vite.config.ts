59f393e5a23dd08a0a75ed0a16b0035d8ee9b47b
HamadMulti <informhammadahmad@gmail.com>
2025-02-20 06:47:01 +0300

    added dotenv packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env,
+  },
   plugins: [
     react(),
     tailwindcss(),
