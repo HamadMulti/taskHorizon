7c95061d7fcbc97de32dff47397832aab0daaf66
HamadMulti <informhammadahmad@gmail.com>
2025-02-20 10:58:59 +0300

    added auto logout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import VerifyOTP from "./pages/VerifyOTP";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import NotFound from "./utils/NotFound";
-import { fetchUserDetails, hydrateAuthState } from "./features/authSlice";
+import { fetchUserDetails, hydrateAuthState, logoutUser } from "./features/authSlice";
 import { useEffect } from "react";
 import { AppDispatch, RootState } from "./app/store";
 import { useDispatch, useSelector } from "react-redux";
@@ -16,10 +16,11 @@ import MyTask from "./pages/dashboard/body/tasks/MyTasks";
 import Task from "./pages/dashboard/body/tasks/Task";
 import Project from "./pages/dashboard/body/projects/Project";
 import Settings from "./pages/dashboard/body/settings/settings";
+import { decodeToken } from "./utils/decodeToken";
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
-  const {user } = useSelector((state: RootState) => state.auth)
+  const {user, token } = useSelector((state: RootState) => state.auth)
 
   useEffect(() => {
     dispatch(hydrateAuthState());
@@ -30,6 +31,27 @@ function App() {
         dispatch(fetchUserDetails())
       }
     }, [dispatch, user]);
+
+    useEffect(() => {
+      if (token) {
+        const decoded = decodeToken(token);
+        if (decoded) {
+          const expiryTime = decoded.exp * 1000;
+          const timeLeft = expiryTime - Date.now();
+          
+          if (timeLeft > 0) {
+            const timer = setTimeout(() => {
+              dispatch(logoutUser())
+              alert('Your session has expired. Please log in again.');
+              window.location.href = '/login';
+            }, timeLeft);
+            
+            return () => clearTimeout(timer);
+          }
+        }
+      }
+    }, [dispatch, token]);
+    
 
   return (
     <Router>
