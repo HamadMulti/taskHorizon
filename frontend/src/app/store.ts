261228dad3509711dfe47c8f643974e694af48e0
HamadMulti <informhammadahmad@gmail.com>
2025-02-20 06:48:12 +0300

    fixed redux store handlers

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import taskReducer from "../features/taskSlice";
 import projectReducer from "../features/projectSlice";
+import { persistStore } from "redux-persist";
 
 export const store = configureStore({
   reducer: {
@@ -9,7 +10,12 @@ export const store = configureStore({
     tasks: taskReducer,
     projects: projectReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
