098d73da2b0d932719e00c314e86dfa11e439348
HamadMulti <informhammadahmad@gmail.com>
2025-02-20 20:07:24 +0300

    added missing user project route

--- backend/routes/project_routes.py
+++ backend/routes/project_routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
-from controllers.project_controller import create_project, get_projects, update_project, delete_project
+from controllers.project_controller import create_project, get_projects, get_user_projects, update_project, delete_project
 
 project_bp = Blueprint("project", __name__)
 
 project_bp.route("/", methods=["POST"])(create_project)
 project_bp.route("/", methods=["GET"])(get_projects)
+project_bp.route("/user", methods=["GET"])(get_user_projects)
 project_bp.route("/<int:project_id>", methods=["PUT"])(update_project)
 project_bp.route("/<int:project_id>", methods=["DELETE"])(delete_project)
